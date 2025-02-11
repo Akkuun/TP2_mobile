deb69e13c1a32db7298f3b734d6173ec5b31de1b
Mathis <90316879+Akkuun@users.noreply.github.com>
2025-02-11 16:26:57 +0100

    WIP

--- app/src/main/java/com/example/tp2_mobile/Exercice1Activity.java
+++ app/src/main/java/com/example/tp2_mobile/Exercice1Activity.java
@@ -1,8 +1,6 @@
 package com.example.tp2_mobile;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button goToExercice1;
+public class Exercice1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        goToExercice1 = findViewById(R.id.Exercice1);
-        goToExercice1.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Exercice1Activity.class);
-            startActivity(intent);
-        });
-
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.exercice1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
-
 }
