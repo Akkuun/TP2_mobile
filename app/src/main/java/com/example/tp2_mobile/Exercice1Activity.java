0540a30685de7df02b75f6600c4d08e4fa555c5c
Mathis <90316879+Akkuun@users.noreply.github.com>
2025-02-18 13:40:54 +0100

    EX1 done

--- app/src/main/java/com/example/tp2_mobile/Exercice1Activity.java
+++ app/src/main/java/com/example/tp2_mobile/Exercice1Activity.java
@@ -1,6 +1,11 @@
 package com.example.tp2_mobile;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class Exercice1Activity extends AppCompatActivity {
+
+    private Button searchDeviceButton;
+    private ScrollView scrollView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.exercice1);
+
+        searchDeviceButton = findViewById(R.id.button2);
+        scrollView = findViewById(R.id.viewDevice);
+        textView = findViewById(R.id.textView);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // When we click on our button, we display in the scroll view all the sensors available on the phone
+        searchDeviceButton.setOnClickListener(v -> {
+            SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+            StringBuilder sensorInfo = new StringBuilder();
+            for (Sensor sensor : deviceSensors) {
+                sensorInfo.append(sensor.getName()).append("\n");
+            }
+
+            if (textView != null) {
+                textView.setText(sensorInfo.toString());
+            }
         });
     }
 }
--- app/src/main/java/com/example/tp2_mobile/MainActivity.java
+++ app/src/main/java/com/example/tp2_mobile/MainActivity.java
@@ -16,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
