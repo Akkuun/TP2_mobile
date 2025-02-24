b3aef646f170bde9eb8347298352473c91277a73
Mathis <90316879+Akkuun@users.noreply.github.com>
2025-02-24 11:43:22 +0100

    layout done

---
+++ app/src/main/java/com/example/tp2_mobile/Exercice8Activity.java
@@ -0,0 +1,41 @@
+package com.example.tp2_mobile;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Exercice8Activity extends AppCompatActivity {
+
+
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.exercice8);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+}
